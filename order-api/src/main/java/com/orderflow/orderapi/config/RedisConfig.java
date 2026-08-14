5f38dd6d80dfc5a64f9973e96cd15272fcd1fe20
emirhan.murat <m.emirhanmurat@gmail.com>
2026-08-14 17:25:34 +0300

    added missing imports to redis conf

--- order-api/src/main/java/com/orderflow/orderapi/config/RedisConfig.java
+++ order-api/src/main/java/com/orderflow/orderapi/config/RedisConfig.java
@@ -6,6 +6,8 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfig {
